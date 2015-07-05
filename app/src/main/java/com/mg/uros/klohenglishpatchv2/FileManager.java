f2bdacff5b8e5e7bf8968d484e67ab822487f771
todorxp <todorads@gmail.com>
2015-07-05 21:49:56 +0200

    Auto update option added

--- app/src/main/java/com/mg/uros/klohenglishpatchv2/FileManager.java
+++ app/src/main/java/com/mg/uros/klohenglishpatchv2/FileManager.java
@@ -7,12 +7,17 @@ import android.content.res.AssetManager;
 import android.util.Log;
 
 
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 public class FileManager {
 
@@ -111,6 +116,89 @@ public class FileManager {
             in.close();
             out.close();
         }
+    }
+
+    public boolean unpackZip(String path, String zipname)
+    {
+        InputStream is;
+        ZipInputStream zis;
+        try
+        {
+            is = new FileInputStream(path + zipname);
+            zis = new ZipInputStream(new BufferedInputStream(is));
+            ZipEntry ze;
+
+            while((ze = zis.getNextEntry()) != null)
+            {
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                byte[] buffer = new byte[1024];
+                int count;
+
+                String filename = ze.getName();
+                FileOutputStream fout = new FileOutputStream(path + filename);
+
+                // reading and writing
+                while((count = zis.read(buffer)) != -1)
+                {
+                    baos.write(buffer, 0, count);
+                    byte[] bytes = baos.toByteArray();
+                    fout.write(bytes);
+                    baos.reset();
+                }
+
+                fout.close();
+                zis.closeEntry();
+            }
+
+            zis.close();
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
+    public  void unzip(File zipFile, File targetDirectory) throws IOException {
+        ZipInputStream zis = new ZipInputStream(
+                new BufferedInputStream(new FileInputStream(zipFile)));
+        try {
+            ZipEntry ze;
+            int count;
+            byte[] buffer = new byte[8192];
+            while ((ze = zis.getNextEntry()) != null) {
+                File file = new File(targetDirectory, ze.getName());
+                File dir = ze.isDirectory() ? file : file.getParentFile();
+                if (!dir.isDirectory() && !dir.mkdirs())
+                    throw new FileNotFoundException("Failed to ensure directory: " +
+                            dir.getAbsolutePath());
+                if (ze.isDirectory())
+                    continue;
+                FileOutputStream fout = new FileOutputStream(file);
+                try {
+                    while ((count = zis.read(buffer)) != -1)
+                        fout.write(buffer, 0, count);
+                } finally {
+                    fout.close();
+                }
+            /* if time should be restored as well
+            long time = ze.getTime();
+            if (time > 0)
+                file.setLastModified(time);
+            */
+            }
+        } finally {
+            zis.close();
+        }
+    }
+
+    void DeleteRecursive(File fileOrDirectory) {
+        if (fileOrDirectory.isDirectory())
+            for (File child : fileOrDirectory.listFiles())
+                DeleteRecursive(child);
+
+        fileOrDirectory.delete();
     }
 
 
--- app/src/main/java/com/mg/uros/klohenglishpatchv2/MainActivity.java
+++ app/src/main/java/com/mg/uros/klohenglishpatchv2/MainActivity.java
@@ -1,6 +1,9 @@
 package com.mg.uros.klohenglishpatchv2;
 
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
@@ -9,11 +12,17 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
+import com.koushikdutta.ion.ProgressCallback;
+
 import java.io.File;
 import java.io.IOException;
-
+import org.apache.commons.io.FileUtils;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -21,9 +30,24 @@ public class MainActivity extends ActionBarActivity {
 
     final static String dataDir = Environment.getExternalStorageDirectory() + "/Android/data/com.nexon.loh.korgooglev1/files/at2/cacheroot/gamedata/mobilebundles/";
     final static String backupDir = Environment.getExternalStorageDirectory() + "/Android/data/com.mg.uros.klohenglishpatchv2/backup/";
+    final static String updatesDir = Environment.getExternalStorageDirectory() + "/Android/data/com.mg.uros.klohenglishpatchv2/updates/";
+    final static String koreanUpdatesDir = Environment.getExternalStorageDirectory() + "/Android/data/com.mg.uros.klohenglishpatchv2/updates/korean";
+    final static String englishUpdatesDir = Environment.getExternalStorageDirectory() + "/Android/data/com.mg.uros.klohenglishpatchv2/updates/english";
+    final static String patch = Environment.getExternalStorageDirectory() + "/Android/data/com.mg.uros.klohenglishpatchv2/updates/patch.zip";
+    final static String tempDir = Environment.getExternalStorageDirectory() + "/Android/data/com.mg.uros.klohenglishpatchv2/temp/";
+    final static String tempPatch = Environment.getExternalStorageDirectory() + "/Android/data/com.mg.uros.klohenglishpatchv2/temp/patch.zip";
+
 
     File dataFolder = new File(dataDir);
     File backupFolder = new File(backupDir);
+    File patchFile = new File(patch);
+    File updateFolder = new File(updatesDir);
+    File tempFolder = new File (tempDir);
+    File tempFile = new File(tempPatch);
+    File koreanUpdatedFolder = new File (koreanUpdatesDir);
+    File englishUpdatedFolder = new File (englishUpdatesDir);
+
+
 
     FileManager fileManager = new FileManager(MainActivity.this);
     SharedPreferences settings = null;
@@ -35,6 +59,8 @@ public class MainActivity extends ActionBarActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
         String PREFS_NAME = "com.mg.uros.klohenglishpatchv2";
         SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
@@ -51,10 +77,19 @@ public class MainActivity extends ActionBarActivity {
             editor.putString("status", "Default");
             editor.commit(); // Save all changed settings
 
+
+
             try {
+                new File(updatesDir).mkdirs();
+                new File(tempDir).mkdirs();
+                new File(koreanUpdatesDir).mkdirs();
+                new File(englishUpdatesDir).mkdirs();
+                fileManager.copyAssets("koreanpatch", updatesDir);
+                fileManager.unzip(patchFile, updateFolder);
                 fileManager.copyDirectory(dataFolder, backupFolder);
             } catch (IOException e) {
                 e.printStackTrace();
+                Toast.makeText(MainActivity.this, "SD card is not present or kloh is not installed", Toast.LENGTH_LONG).show();
             }
 
         } else {
@@ -63,11 +98,6 @@ public class MainActivity extends ActionBarActivity {
 
 
            statusValue.setText(getStatus);
-
-
-
-
-
 
         }
 
@@ -100,34 +130,46 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void onEnglishPatchClick(View view) {
+        try {
+            fileManager.copyDirectory(englishUpdatedFolder,dataFolder);
+            Toast.makeText(MainActivity.this,"You have successfully applied English patch",Toast.LENGTH_LONG).show();
 
-        fileManager.copyAssets("englishpatch", dataDir);
-        Toast.makeText(MainActivity.this,"You have successfully applied English patch",Toast.LENGTH_LONG).show();
+            String PREFS_NAME = "com.mg.uros.klohenglishpatchv2";
+            SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+            TextView statusValue =  (TextView) findViewById(R.id.status_text_value);
 
-        String PREFS_NAME = "com.mg.uros.klohenglishpatchv2";
-        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
-        TextView statusValue =  (TextView) findViewById(R.id.status_text_value);
+            statusValue.setText("English");
 
-        statusValue.setText("English");
+            SharedPreferences.Editor editor = settings.edit();
+            editor.putString("status", "English");
+            editor.commit();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(MainActivity.this, "You have failed to apply English patch", Toast.LENGTH_LONG).show();
+        }
 
-        SharedPreferences.Editor editor = settings.edit();
-        editor.putString("status", "English");
-        editor.commit();
     }
 
     public void onRestoreKoreanClick(View view) {
 
-        fileManager.copyAssets("koreanpatch", dataDir);
-        Toast.makeText(MainActivity.this,"You have successfully restored Korean files",Toast.LENGTH_LONG).show();
+        try {
+            fileManager.copyDirectory(koreanUpdatedFolder,dataFolder);
+            Toast.makeText(MainActivity.this,"You have successfully restored Korean files",Toast.LENGTH_LONG).show();
+            String PREFS_NAME = "com.mg.uros.klohenglishpatchv2";
+            SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+            TextView statusValue =  (TextView) findViewById(R.id.status_text_value);
+            statusValue.setText("Korean");
 
-        String PREFS_NAME = "com.mg.uros.klohenglishpatchv2";
-        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
-        TextView statusValue =  (TextView) findViewById(R.id.status_text_value);
-        statusValue.setText("Korean");
+            SharedPreferences.Editor editor = settings.edit();
+            editor.putString("status", "Korean");
+            editor.commit();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(MainActivity.this, "You have failed to restore korean files", Toast.LENGTH_LONG).show();
+        }
 
-        SharedPreferences.Editor editor = settings.edit();
-        editor.putString("status", "Korean");
-        editor.commit();
+
+
 
     }
 
@@ -145,6 +187,7 @@ public class MainActivity extends ActionBarActivity {
             editor.commit();
 
         } catch (IOException e) {
+            Toast.makeText(MainActivity.this, "You have failed to restore backup files", Toast.LENGTH_LONG).show();
             e.printStackTrace();
         }
     }
@@ -152,11 +195,117 @@ public class MainActivity extends ActionBarActivity {
     public void onBackupFilesClick(View view) {
 
         try {
-            fileManager.copyDirectory(dataFolder,backupFolder);
+            fileManager.copyDirectory(dataFolder, backupFolder);
             Toast.makeText(MainActivity.this,"You have successfully made backup of your files",Toast.LENGTH_LONG).show();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+    }
+
+
+    public void alertDialog(final Boolean result)
+    {
+        AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+
+        if (result)
+        {
+            builder1.setTitle("Updates not found !");
+            builder1.setMessage("You have the latest version of translation files");
+        }
+        else
+        {
+            builder1.setTitle("New update found !");
+            builder1.setMessage("Click OK to get new updated translation files");
+        }
+
+
+        builder1.setCancelable(true);
+
+        builder1.setNeutralButton(android.R.string.ok,
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+
+                        if (!result)
+                        {
+                            try {
+
+                                boolean deleted = patchFile.delete();
+                                fileManager.copyDirectory(tempFolder,updateFolder);
+                                deleted = tempFile.delete();
+                                fileManager.DeleteRecursive(koreanUpdatedFolder);
+                                fileManager.DeleteRecursive(englishUpdatedFolder);
+                                new File(koreanUpdatesDir).mkdirs();
+                                new File(englishUpdatesDir).mkdirs();
+                                fileManager.unzip(patchFile,updateFolder);
+                                Toast.makeText(MainActivity.this, "You have successfully updated translation files, use patch option again", Toast.LENGTH_LONG).show();
+
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                                Toast.makeText(MainActivity.this, "You have failed to update translation files", Toast.LENGTH_LONG).show();
+                            }
+
+                        }
+                    }
+                });
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
+
+    }
+
+    public void onCheckUpdateClick(View view) {
+
+
+        final  ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
+        progressDialog.setTitle("Updates");
+        progressDialog.setMessage("Checking for updates.....Please wait....");
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        final ProgressBar progressBar = new ProgressBar(MainActivity.this);
+        progressDialog.show();
+
+        // http://download1515.mediafire.com/jnctcl7clfgg/olon1araglodi5n/patch.zip
+        // "http://metal-genesis.com/wp-content/uploads/2015/07/patch.zip"
+
+        Ion.with(MainActivity.this)
+                .load("http://metal-genesis.com/wp-content/uploads/2015/07/patch.zip")
+                // have a ProgressBar get updated automatically with the percent
+                .progressBar(progressBar)
+                // and a ProgressDialog
+                .progressDialog(progressDialog)
+                // can also use a custom callback
+                .progress(new ProgressCallback() {
+                    @Override
+                    public void onProgress(long downloaded, long total) {
+                        System.out.println("" + downloaded + " / " + total);
+
+                        progressDialog.setMax((int)total);
+                        progressDialog.setProgress((int)downloaded);
+
+
+                    }
+                })
+                .write(new File(tempPatch))
+                .setCallback(new FutureCallback<File>() {
+                    @Override
+                    public void onCompleted(Exception e, File file) {
+                        // download done...
+                        // do stuff with the File or error
+                        progressDialog.cancel();
+
+                        try {
+                            boolean compare1and2 = FileUtils.contentEquals(patchFile, file);
+                            Log.v("compare", String.valueOf(compare1and2));
+
+                            alertDialog(compare1and2);
+
+
+                        } catch (IOException e2) {
+                            e2.printStackTrace();
+                            Toast.makeText(MainActivity.this, "You have failed to check for updates", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
     }
 }
